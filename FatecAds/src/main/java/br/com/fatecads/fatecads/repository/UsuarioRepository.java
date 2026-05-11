56958ef7361606b6ea22ac7c239a9faa3e7b56de
stafuza <enzo.stafuza@gmail.com>
2026-05-11 19:58:57 -0300

    implementação de usuario

---
+++ FatecAds/src/main/java/br/com/fatecads/fatecads/repository/UsuarioRepository.java
@@ -0,0 +1,14 @@
+package br.com.fatecads.fatecads.repository;
+
+import br.com.fatecads.fatecads.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+
+    Optional<Usuario> findByLoginUsuario(String loginUsuario);
+    
+}
