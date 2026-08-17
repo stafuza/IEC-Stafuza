f71154d06ed9d21c5abaadc90a97e3a349a05087
stafuza <enzo.stafuza@gmail.com>
2026-08-17 17:43:33 -0300

    aula 1708

--- FatecAds/src/main/java/br/com/fatecads/fatecads/controller/ProdutoController.java
+++ FatecAds/src/main/java/br/com/fatecads/fatecads/controller/ProdutoController.java
@@ -1,12 +1,19 @@
 package br.com.fatecads.fatecads.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.convert.ReadingConverter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import br.com.fatecads.fatecads.entity.Produto;
 import br.com.fatecads.fatecads.service.ProdutoService;
+import ch.qos.logback.core.model.Model;
 
 @Controller
 @RequestMapping("/produtos")
@@ -20,6 +27,34 @@ public class ProdutoController {
     public String listar(Model model) {
         List<Produto> produtos = produtoService.findAll();
         model.addAttribute("produtos", produtos);
-        return "produtos/listar";
+        return "produtos/listarProdutos";
+    }
+
+    //MÉTODO PARA ABRIR O FORMULÁRIO DE CRIAÇÃO DE UM NOVO PRODUTO
+    @GetMapping("/criar")
+    public String criarForm(Model model) {
+        model.addAttribute("produto", new Produto());
+        return "produtos/novoProduto";
+    }
+    //MÉTODO PARA SALVAR UM NOVO PRODUTO
+    @PostMapping("/salvar")
+    public String salvar(@ModelAttribute Produto produto) {
+        produtoService.save(produto);
+        return "redirect:/produtos/listar";
+    }
+
+    //MÉTODO PARA ABRIR O FORMULÁRIO DE EDIÇÃO DE UM PRODUTO
+    @GetMapping("/editar/{id}")
+    public String editarForm(@PathVariable Integer id, Model model) {
+        Produto produto = produtoService.findById(id);
+        model.addAttribute("produto", produto);
+        return "produtos/formulario";
+    }
+
+    //MÉTODO PARA EXCLUIR UM PRODUTO
+    @GetMapping("/excluir/{id}")
+    public String excluir(@PathVariable Integer id) {
+        produtoService.deleteById(id);
+        return "redirect:/produtos/listar";
     }
 }
